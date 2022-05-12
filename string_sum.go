40179c881f3610469ba98f92343712e4bc9dad99
IrakliGiorgadze <ika.giorgadze@gmail.com>
2022-05-12 18:35:15 +0400

    add solution

--- string_sum.go
+++ string_sum.go
@@ -4,24 +4,28 @@ import (
 	"errors"
 )
 
-//use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
-	// Use when the input is empty, and input is considered empty if the string contains only whitespace
-	errorEmptyInput = errors.New("input is empty")
-	// Use when the expression has number of operands not equal to two
+	errorEmptyInput     = errors.New("input is empty")
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
 )
 
-// Implement a function that computes the sum of two int numbers written as a string
-// For example, having an input string "3+5", it should return output string "8" and nil error
-// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
-//
-//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
-// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
-// with fmt.Errorf function
-//
-// Use the errors defined above as described, again wrapping into fmt.Errorf
+func StringSum(input string, input2 string) (output string, err error) {
+	//add 2 strings together and return the result as a string  if the input is empty, return an error
+	if input == "" {
+		err = errorEmptyInput
+		return
+	}
+	if input2 == "" {
+		err = errorEmptyInput
+		return
+	}
+	if len(input) != len(input2) {
+		err = errorNotTwoOperands
+		return
+	}
+	for i := 0; i < len(input); i++ {
+		output += string(input[i] + input2[i])
+	}
 
-func StringSum(input string) (output string, err error) {
 	return "", nil
 }
