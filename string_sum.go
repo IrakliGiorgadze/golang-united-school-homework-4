70862187b34e11ab2ddcdeb0f753907fe6f1d7a2
IrakliGiorgadze <ika.giorgadze@gmail.com>
2022-05-13 23:46:59 +0400

    add solution v3

--- string_sum.go
+++ string_sum.go
@@ -3,7 +3,6 @@ package string_sum
 import (
 	"errors"
 	"strconv"
-	"unicode"
 )
 
 var (
@@ -12,27 +11,56 @@ var (
 )
 
 func StringSum(input string) (output string, err error) {
+	//convert input to runes
 	runes := []rune(input)
 
-	if len(runes) == 0 {
+	//convert runes to array of characters
+	chars := make([]string, len(runes))
+
+	for i, r := range runes {
+		chars[i] = string(r)
+	}
+
+	//use isDigit to check if character is a digit
+	isDigit := func(c string) bool {
+		_, err := strconv.Atoi(c)
+		return err == nil
+	}
+
+	//use isOperator to check if character is an operator
+	isOperator := func(c string) bool {
+		return c == "+" || c == "-"
+	}
+
+	//use isSpace to check if character is a space
+	isSpace := func(c string) bool {
+		return c == " "
+	}
+
+	//calculate sum of digits
+	sum := func(digits []string) (int, error) {
+		var sum int
+		for _, d := range digits {
+			i, err := strconv.Atoi(d)
+			if err != nil {
+				return 0, err
+			}
+			sum += i
+		}
+		return sum, nil
+	}
+
+	//check if input is empty
+	if len(chars) == 0 {
 		return "", errorEmptyInput
 	}
 
-	for _, r := range runes {
-		if !unicode.IsDigit(r) {
+	//check if input contains only digits and operators
+	for _, c := range chars {
+		if !isDigit(c) && !isOperator(c) && !isSpace(c) {
 			return "", errorNotTwoOperands
 		}
 	}
 
-	integers := make([]int, len(runes))
-	for i, r := range runes {
-		integers[i], _ = strconv.Atoi(string(r))
-	}
-
-	sum := 0
-	for _, i := range integers {
-		sum += i
-	}
-
-	return strconv.Itoa(sum), nil
+	return output, nil
 }
