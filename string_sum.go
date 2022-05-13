77da5f72e84a0fc1b28dd690da713fb13bcc0234
IrakliGiorgadze <ika.giorgadze@gmail.com>
2022-05-13 23:53:56 +0400

    add solution v4

--- string_sum.go
+++ string_sum.go
@@ -11,56 +11,23 @@ var (
 )
 
 func StringSum(input string) (output string, err error) {
-	//convert input to runes
 	runes := []rune(input)
 
-	//convert runes to array of characters
 	chars := make([]string, len(runes))
 
 	for i, r := range runes {
 		chars[i] = string(r)
 	}
 
-	//use isDigit to check if character is a digit
-	isDigit := func(c string) bool {
-		_, err := strconv.Atoi(c)
-		return err == nil
-	}
-
-	//use isOperator to check if character is an operator
-	isOperator := func(c string) bool {
-		return c == "+" || c == "-"
-	}
-
-	//use isSpace to check if character is a space
-	isSpace := func(c string) bool {
-		return c == " "
-	}
-
-	//calculate sum of digits
-	sum := func(digits []string) (int, error) {
-		var sum int
-		for _, d := range digits {
-			i, err := strconv.Atoi(d)
-			if err != nil {
-				return 0, err
-			}
-			sum += i
-		}
-		return sum, nil
-	}
-
-	//check if input is empty
-	if len(chars) == 0 {
-		return "", errorEmptyInput
-	}
-
-	//check if input contains only digits and operators
+	sum := 0
 	for _, c := range chars {
-		if !isDigit(c) && !isOperator(c) && !isSpace(c) {
-			return "", errorNotTwoOperands
+		num, err := strconv.Atoi(c)
+		if err != nil {
+			return "", err
 		}
+		sum += num
 	}
 
-	return output, nil
+	return strconv.Itoa(sum), nil
+
 }
